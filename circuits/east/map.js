d680c732fde3b3348ea7720d13265c48f2a12372
Kirito <LoickEmadesemadisson@gmail.com>
2023-08-15 16:32:58 +0300

    north

--- circuits/east/map.js
+++ circuits/east/map.js
@@ -11,9 +11,10 @@ const sainte_marie = [-17.0911, 49.8152];
 const Tamatave = [-18.1498, 49.4029];
 const andasibé= [-18.9150,48.4283];
 const mahambo= [-16.3039,49.7661];
+const Palmarium= [-16.0256,49.7661,49.6690];
 
 
-const places = [
+const places= [
   {
     name: 'Antananarivo',
     location: antananarivo,
@@ -29,28 +30,35 @@ const places = [
     location: Tamatave,
     description: 'Toamasina est le plus grand port de Madagascar et joue un rôle vital dans l"économie du pays',
   },
-  {
-    name: 'andasibé',
-    location: andasibé,
-    description: 'Andasibe est réputée pour sa réserve spéciale d"Analamazaotra (Parc National de Mantadia',
-  },
+ 
 
   {
     name: 'mahambo',
     location: mahambo,
     description: 'Mahambo est une destination populaire pour les surfeurs et les voyageurs à la recherche de plages tranquilles et d"une ambiance détendue',
   }
-
-
+  ,
+  {
+    name: 'Palmarium',
+    location: Palmarium,
+    description: 'île de sainte-Marie',
+}
+  
 ]
+
+ 
+
+
+
+
 
 
   // Initialiser la carte
   const map = L.map('map').setView(antananarivo, 7.4);
 
   // Ajouter la couche de carte
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
   }).addTo(map);
 
 map.scrollWheelZoom.disable();
@@ -73,20 +81,12 @@ map.on('mousewheel', function (e) {
   // marqueur
   places.forEach(place => {
     const marker = L.marker(place.location).addTo(map);
-    marker.bindPopup(`<b>${place.name}</b><br>${place.description}`);
+    marker.bindPopup(`<b>${place.name}</b>`);
   });
 
-  addMarker(antananarivo, 'Antananarivo');
-  addMarker(andasibé, 'Andasibe');
-  addMarker(mahambo, 'Mahambo');
-  addMarker(sainte_marie, 'Sainte-Marie');
-  addMarker(Tamatave, 'Tamatave');
-  const ant=[antananarivo,andasibé]
-
-  L.polyline(ant, { color: 'blue' }).addTo(map);
-    L.polyline([andasibe, mahambo], { color: 'green' }).addTo(map);
-    L.polyline([mahambo, sainteMarie], { color: 'red' }).addTo(map);
   
+
+
 
 
     map.setZoomEnabled(false);
--- circuits/south/map.js
+++ circuits/south/map.js
@@ -1,4 +1,85 @@
 
+  
+//   // Coordonnées de Madagascar Nord
+//   const latitude = -12.2800;
+//     const longitude =  49.2917;
+
+    
+
+//   // Initialiser la carte
+//   const Antananarivo = [-18.8792,47.5079 ];
+// const Antsirabé= [-19.8691, 47.0328 ];
+// const Ambositra = [-20.5151, 47.2522];
+// const Ranomafana = [-21.2569, 47.4109];
+// const Fianarantsoa = [-21.4414, 47.0844];
+// const Ambalavao = [-21.9129, 46.9395];
+
+
+// const places = [
+//   {
+//     name: 'Antananarivo',
+//     location: Antananarivo,
+//   },
+//   {
+//     name: 'Antsirabé',
+//     location: Antsirabé,
+//   },
+//   {
+//     name: 'Ambositra',
+//     location: Ambositra,
+//   },
+//   {
+//     name: 'Ranomafana',
+//     location: Ranomafana,
+//   },
+
+//   {
+//     name: 'Fianarantsoa',
+//     location: Fianarantsoa,
+//   },
+//   {
+//     name: 'Ambalavao',
+//     location: Ambalavao,
+//   }
+
+
+// ]
+
+
+//   // Initialiser la carte
+//   const map = L.map('map').setView(antananarivo, 7.4);
+
+ 
+
+//   map.scrollWheelZoom.disable();
+//   map.touchZoom.disable();
+  
+//   // Gestionnaire d'événement pour la molette de la souris
+//   map.on('mousewheel', function (e) {
+//       // Empêcher le zoom en annulant l'événement
+//       e.preventDefault();
+//       e.stopPropagation();
+      
+//   });
+
+
+//   places.forEach(place => {
+//     const marker = L.marker(place.location).addTo(map);
+//     marker.bindPopup(`<b>${place.name}</b>`);
+//   });
+
+
+   
+    
+  
+
+
+
+  
+  
+  
+  
+  
   // Coordonnées de Madagascar Nord
   const latitude = -12.2800;
     const longitude =  49.2917;
@@ -6,39 +87,39 @@
     
 
   // Initialiser la carte
-  const antananarivo = [-18.8792,47.5079 ];
-const sainte_marie = [-17.0911, 49.8152];
-const Tamatave = [-18.1498, 49.4029];
-const andasibé= [-18.9150,48.4283];
-const mahambo= [-16.3039,49.7661];
-
+  const Antananarivo = [-18.8792,47.5079 ];
+  const Antsirabé= [-19.8691, 47.0328 ];
+  const Ambositra = [-20.5151, 47.2522];
+  const Ranomafana = [-21.2569, 47.4109];
+   const Fianarantsoa = [-21.4414, 47.0844];
+   const Ambalavao = [-21.9129, 46.9395];
 
 const places = [
   {
     name: 'Antananarivo',
-    location: antananarivo,
-    description: 'Capitale de Madagascar',
+    location: Antananarivo,
   },
   {
-    name: 'Sainte-marie',
-    location: sainte_marie,
-    description: 'île de sainte-Marie',
+    name: 'Antsirabé',
+    location: Antsirabé,
   },
   {
-    name: 'Tamatave',
-    location: Tamatave,
-    description: 'Toamasina est le plus grand port de Madagascar et joue un rôle vital dans l"économie du pays',
+    name: 'Ambositra',
+    location: Ambositra,
   },
   {
-    name: 'andasibé',
-    location: andasibé,
-    description: 'Andasibe est réputée pour sa réserve spéciale d"Analamazaotra (Parc National de Mantadia',
+    name: 'Ranomafana',
+    location: Ranomafana,
   },
 
   {
-    name: 'mahambo',
-    location: mahambo,
-    description: 'Mahambo est une destination populaire pour les surfeurs et les voyageurs à la recherche de plages tranquilles et d"une ambiance détendue',
+    name: 'Fianarantsoa',
+    location:Fianarantsoa,
+  },
+
+  {
+    name: 'Ambalavao',
+    location:Ambalavao,
   }
 
 
@@ -46,7 +127,7 @@ const places = [
 
 
   // Initialiser la carte
-  const map = L.map('map').setView(antananarivo, 7.4);
+  const map = L.map('map').setView(Antananarivo, 7.4);
 
   // Ajouter la couche de carte
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -56,19 +137,23 @@ const places = [
   // marqueur
   places.forEach(place => {
     const marker = L.marker(place.location).addTo(map);
-    marker.bindPopup(`<b>${place.name}</b><br>${place.description}`);
+    marker.bindPopup(`<b>${place.name}</b>`);
   });
 
-  addMarker(antananarivo, 'Antananarivo');
-  addMarker(andasibé, 'Andasibe');
-  addMarker(mahambo, 'Mahambo');
-  addMarker(sainte_marie, 'Sainte-Marie');
-  addMarker(Tamatave, 'Tamatave');
-  const ant=[antananarivo,andasibé]
 
-  L.polyline(ant, { color: 'blue' }).addTo(map);
-    L.polyline([andasibe, mahambo], { color: 'green' }).addTo(map);
-    L.polyline([mahambo, sainteMarie], { color: 'red' }).addTo(map);
+  map.scrollWheelZoom.disable();
+map.touchZoom.disable();
+
+// Gestionnaire d'événement pour la molette de la souris
+map.on('mousewheel', function (e) {
+    // Empêcher le zoom en annulant l'événement
+    e.preventDefault();
+    e.stopPropagation();
+    
+    // Vous pouvez également insérer ici votre propre logique
+    // pour changer la position ou l'aspect de la carte en réponse à la molette
+});
+
   
 
 
