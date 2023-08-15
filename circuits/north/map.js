a3498657f699bf06448e08ea6a3ede0981ab4fbf
Kirito <LoickEmadesemadisson@gmail.com>
2023-08-15 17:44:35 +0300

    map north

--- circuits/north/map.js
+++ circuits/north/map.js
@@ -7,38 +7,65 @@
 
   // Initialiser la carte
   const antananarivo = [-18.8792,47.5079 ];
-const sainte_marie = [-17.0911, 49.8152];
-const Tamatave = [-18.1498, 49.4029];
-const andasibé= [-18.9150,48.4283];
-const mahambo= [-16.3039,49.7661];
+  const Ankarafantsika = [-16.3000, 46.8000];
+  const Ankarana = [-12.8333, 49.1667];
+  const PortBerger = [-14.9500, 50.0167];
+  const Antsohihy = [-14.8799, 47.9873];
+  const Ambanja = [-13.6882, 48.4502];
+  const MontagneAmbre = [-12.5105, 49.1720];
+  const DiegoSuarez = [-12.2800, 49.2919];
+  const NosyBe = [-13.3239, 48.2683];
+
+
+
+
+
+
+
+
 
 
 const places = [
   {
     name: 'Antananarivo',
     location: antananarivo,
-    description: 'Capitale de Madagascar',
   },
   {
-    name: 'Sainte-marie',
-    location: sainte_marie,
-    description: 'île de sainte-Marie',
+    name: 'Ankarafantsika',
+    location: Ankarafantsika,
   },
   {
-    name: 'Tamatave',
-    location: Tamatave,
-    description: 'Toamasina est le plus grand port de Madagascar et joue un rôle vital dans l"économie du pays',
+    name: 'Ankarana',
+    location: Ankarana,
   },
   {
-    name: 'andasibé',
-    location: andasibé,
-    description: 'Andasibe est réputée pour sa réserve spéciale d"Analamazaotra (Parc National de Mantadia',
+    name: 'Port Berger',
+    location: PortBerger,
   },
 
   {
-    name: 'mahambo',
-    location: mahambo,
-    description: 'Mahambo est une destination populaire pour les surfeurs et les voyageurs à la recherche de plages tranquilles et d"une ambiance détendue',
+    name: 'Antsohihy',
+    location: Antsohihy,
+  },
+  {
+    name: 'Ambanja',
+    location: Ambanja,
+  },
+  {
+    name: 'Antsohihy',
+    location: Antsohihy,
+  },
+  {
+    name: 'Montagne d" Ambre',
+    location: MontagneAmbre,
+  },
+  {
+    name: 'Diego Suarez',
+    location: DiegoSuarez,
+  },
+  {
+    name: 'Nosy-Be',
+    location: NosyBe,
   }
 
 
@@ -46,29 +73,34 @@ const places = [
 
 
   // Initialiser la carte
-  const map = L.map('map').setView(antananarivo, 7.4);
+  const map = L.map('map').setView(Ambanja, 7.4);
 
   // Ajouter la couche de carte
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
   }).addTo(map);
 
+
+  map.scrollWheelZoom.disable();
+map.touchZoom.disable();
+
+// Gestionnaire d'événement pour la molette de la souris
+map.on('mousewheel', function (e) {
+    // Empêcher le zoom en annulant l'événement
+    e.preventDefault();
+    e.stopPropagation();
+    
+    // Vous pouvez également insérer ici votre propre logique
+    // pour changer la position ou l'aspect de la carte en réponse à la molette
+});
+
+
   // marqueur
   places.forEach(place => {
     const marker = L.marker(place.location).addTo(map);
-    marker.bindPopup(`<b>${place.name}</b><br>${place.description}`);
+    marker.bindPopup(`<b>${place.name}</b>`);
   });
 
-  addMarker(antananarivo, 'Antananarivo');
-  addMarker(andasibé, 'Andasibe');
-  addMarker(mahambo, 'Mahambo');
-  addMarker(sainte_marie, 'Sainte-Marie');
-  addMarker(Tamatave, 'Tamatave');
-  const ant=[antananarivo,andasibé]
-
-  L.polyline(ant, { color: 'blue' }).addTo(map);
-    L.polyline([andasibe, mahambo], { color: 'green' }).addTo(map);
-    L.polyline([mahambo, sainteMarie], { color: 'red' }).addTo(map);
   
 
 
--- circuits/south/map.js
+++ circuits/south/map.js
@@ -92,7 +92,17 @@
   const Ambositra = [-20.5151, 47.2522];
   const Ranomafana = [-21.2569, 47.4109];
    const Fianarantsoa = [-21.4414, 47.0844];
-   const Ambalavao = [-21.9129, 46.9395];
+   const Ambalavao=[-21.9129, 46.9395] ;
+   const Isalo = [-22.4500, 45.2833];
+   const Ifaty = [-23.1625, 43.6338];
+   const Tuléar= [-23.3500, 43.6667];
+   const Manakara = [-22.1333, 48.0167];
+   const Andriampotsy=[-22.5084, 46.8444];
+   const Andringitra=[-22.1546, 46.8257];
+   const peak_boby=[-22.6653, 46.9081];
+   const diavolana=[-18.3361, 47.2263];
+   const anakao=[-23.3500, 44.3833] ;
+   
 
 const places = [
   {
@@ -117,9 +127,51 @@ const places = [
     location:Fianarantsoa,
   },
 
+  
+
   {
     name: 'Ambalavao',
     location:Ambalavao,
+  },
+
+  {
+    name: 'Isalo',
+    location:Isalo,
+  },
+  {
+    name: 'Ifaty',
+    location:Ifaty,
+  },
+  {
+    name: 'Tuléar',
+    location:Tuléar,
+  },
+ 
+  {
+    name: 'Manakara',
+    location:Manakara,
+  },
+  {
+    name: 'Andriampotsy',
+    location:Andriampotsy,
+
+  },
+  {
+    name: 'Andringitra',
+    location:Andringitra
+  },
+
+  {
+    name: 'Peak boby',
+    location:peak_boby,
+   },
+   {
+    name: 'Diavolana',
+    location:diavolana,
+  },
+  {
+    name: 'Anakao',
+    location:anakao,
   }
 
 
@@ -127,7 +179,7 @@ const places = [
 
 
   // Initialiser la carte
-  const map = L.map('map').setView(Antananarivo, 7.4);
+  const map = L.map('map').setView(Isalo, 7.4);
 
   // Ajouter la couche de carte
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
